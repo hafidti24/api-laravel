4036db3dfc11f1d1a4b9cfed7a58764bbe2d5494
hafidti24 <hafid.web1ti24b@gmail.com>
2025-10-20 16:42:35 +0700

    todo feature

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/health', fn() => response()->json(['status' => 'ok']));
+Route::apiResource('todos', TodoController::class);
